32b5b07c731c98a09f491d9a7c6975dfcc79c6b5
Benjamin Ehrlich <bo.ehrlich@gmail.com>
2026-05-18 00:08:08 +0300

    feat: exercises 1 and 2 - Day 1

--- Week1/Day1/ExerciseXP/Exercise1.py
+++ Week1/Day1/ExerciseXP/Exercise1.py
@@ -1,4 +1,3 @@
 # Exercise 1: Hello World
 # Print "Hello world" 4 times in one line of code
 print("Hello world\n" * 4)
-
---
+++ Week1/Day1/ExerciseXP/Exercise2.py
@@ -0,0 +1,3 @@
+# Exercise 2: Some Math
+# Calculate (99^3) * 8
+print((99 ** 3) * 8)
