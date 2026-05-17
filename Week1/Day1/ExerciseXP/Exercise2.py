487a072ec74a738f8629968774e10b558ba6780d
Benjamin Ehrlich <bo.ehrlich@gmail.com>
2026-05-18 00:11:54 +0300

    refactor: regroupe tous les exos Day 1 dans un seul fichier

--- Week1/Day1/ExerciseXP/Exercise2.py
+++
@@ -1,3 +0,0 @@
-# Exercise 2: Some Math
-# Calculate (99^3) * 8
-print((99 ** 3) * 8)
---
+++ Week1/Day1/ExerciseXP/ExercisesXP.py
@@ -0,0 +1,24 @@
+# ============================================
+# DI Bootcamp - Week 1 - Day 1 - Exercises XP
+# Author: Benjamin Ehrlich
+# ============================================
+
+
+# ----------------------------------------------
+# Exercise 1: Hello World
+# Instructions:
+# Print "Hello world" 4 times in one line of code
+# ----------------------------------------------
+print("Hello world\n" * 4)
+
+
+# ----------------------------------------------
+# Exercise 2: Some Math
+# Instructions:
+# Calculate (99^3)*8
+# ----------------------------------------------
+print((99 ** 3) * 8)
+
+
+# ----------------------------------------------
+# Exercise 3: ...
